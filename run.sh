f28e0a227947e1578bb49e009e937411220f8299
menobista <divyar@iucaa.in>
2023-04-12 03:27:02 +0530

    updated

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 # running the measurement code
 
 module load anaconda2 mpich-3.3.1 gcc-8.2.0 gsl-2.6
-python count_pairs.py --output pairs.dat 
-python count_pairs.py --output pairs.dat  --mirror True
+#python count_pairs.py --output pairs.dat 
+#python count_pairs.py --output pairs.dat  --mirror True
 python count_pairs.py --output random_pairs.dat --srcrand True 
 python count_pairs.py --output random_pairs.dat --srcrand True --mirror True
 python 2pf.py
